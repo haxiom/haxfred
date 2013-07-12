467ca9fd99f36cde3c133b48c72e837a25976f75
Nick Tomlin <nick.tomlin@gmail.com>
2013-07-12 11:30:42 -0500

    basic messaging support

--- index.js
+++ index.js
@@ -6,6 +6,47 @@ var config = {
   channels: ['#haxiom']
 };
 
+
+/* ==========================================================================
+   helpers
+   ========================================================================== */
+function _rand (min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+/**
+ * Returns a random phrase
+ * @return string
+ * @todo: make this a closure/object that keeps track of which responses are given,
+ * making sure it doesn't give the same response in a row
+ */
+var randPhrase = function () {
+  var phrases = [
+    "The battleline between good and evil runs through the heart of every man.",
+    "How can you expect a man who's warm to understand one who's cold?",
+    "Nice weather, eh?",
+    "<whistles yankee doodle dandee>",
+    "Get your facts first, then you can distort them as you please.",
+    "Only those who will risk going too far can possibly find out how far one can go.",
+    "St. Aquinas was a perl monk",
+    "Ruby is for hipsters",
+    "Totally",
+  ];
+
+  return phrases[_rand(0, phrases.length - 1)];
+}
+
+
+/* Regex
+=================================*/
+// is bot being addressed?
+var personalChat = new RegExp('(^' + config.nick + ')(:.*)');
+
+/* ==========================================================================
+   Chat
+   ========================================================================== */
+
 var client = new irc.Client(config.server, config.nick, {
     channels: config.channels
 });
@@ -24,7 +65,19 @@ client.addListener('join', function (channel, nick) {
 });
 
 
+client.addListener('message' + config.channels[0], function (from, message){
+  // might get a little word, but it's handy for seeing what messages actually crashed the bot
+  console.log('from/to %s ', from, message);
+  var groups = message.match(personalChat);
+
+  if (groups && groups[1] === config.nick) {
+    console.log('matching group: %s', groups[1]);
+    // ends up in a PM pane
+    client.say(config.channels[0], randPhrase());
+  }
+});
+
+
 client.addListener('error', function(message) {
     console.log('error: ', message);
 });
-
