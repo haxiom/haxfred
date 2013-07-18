98019fa632e5c20e60ba1467331311d504db08f2
Nick Tomlin <nick.tomlin@gmail.com>
2013-07-18 12:31:14 -0500

    add new phrases + todos

--- index.js
+++ index.js
@@ -15,13 +15,18 @@ var config = {
 /**
  * Returns a random phrase
  * @return string
- * @todo: make this a closure/object that keeps track of which responses are given,
+ * @todo make this a closure/object that keeps track of which responses are given,
+ * @todo store responses in a JSON file/feed ?
  * making sure it doesn't give the same response in a row
  */
 var randPhrase = function () {
+  //@todo make these contextual? (as in, if there is a '?' at the end of a prompted message, dispatch 'answers to questions')
+  // use a "hash": responses{ '?': ['yes, I think so', 'no way man'], '!' ["i'm excited too"] }
   var phrases = [
+    // Aleksandr Solzhenitsyn
     "The battleline between good and evil runs through the heart of every man.",
     "How can you expect a man who's warm to understand one who's cold?",
+    // haxiom
     "Nice weather, eh?",
     "<whistles yankee doodle dandee>",
     "Get your facts first, then you can distort them as you please.",
@@ -29,6 +34,15 @@ var randPhrase = function () {
     "St. Aquinas was a perl monk",
     "Ruby is for hipsters",
     "Totally",
+    // richard stallman
+   "I see nothing unethical in the job it does. Why shouldn't you send a copy of some music to a friend?",
+   "I'm always happy when I'm protesting.",
+   "'Free software' is a matter of liberty, not price. To understand the concept, you should think of 'free' as in 'free speech,' not as in 'free beer'.",
+   "For personal reasons, I do not browse the web from my computer. (I also have not net connection much of the time.) To look at page I send mail to a demon which runs wget and mails the page back to me. It is very efficient use of my time, but it is slow in real time.",
+   "Playfully doing something difficult, whether useful or not, that is hacking.",
+   "Copying all or parts of a program is as natural to a programmer as breathing, and as productive. It ought to be as free." ,
+   // john lennon
+   "All you need is love."
   ];
 
   return phrases[_.random(0, phrases.length - 1)];
