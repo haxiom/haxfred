58bb1244d6636b8f09895e29aecbdc2d7546337d
Nick Tomlin <nick.tomlin@gmail.com>
2014-02-26 16:31:01 -0800

    Reorganize some irc functionality into haxfred module

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-var nconf = require('nconf');
+var _ = require('lodash');
+
+var Haxfred = require('./lib/haxfred');
+var descend = require('./lib/descend')
+// this is hardcoded. We should use something like nconf to switch between environments
+var config = require('./config/development.json');
+
+haxfred = new Haxfred(config);
+
+// populate haxfred router with commands
+descend()
+  .wrap(haxfred.router)
+  .explore(commands);
+
+haxfred.connect();
+
+
 
