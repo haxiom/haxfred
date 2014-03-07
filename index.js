5c0fede780b483b94908730d97d5f601ffefc957
ntomlin <nick.tomlin@gmail.com>
2014-03-06 19:05:05 -0600

    Cleanup

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var _ = require('lodash');
 
 var Haxfred = require('./lib/haxfred');
 var descend = require('./lib/descend')
+var haxfredIrc = require('./lib/connectors/haxfred-irc');
+var haxfredStd = require('./lib/connectors/haxfred-std');
 // this is hardcoded. We should use something like nconf to switch between environments
 var config = require('./config/development.json');
 
@@ -12,7 +14,5 @@ descend()
   .wrap(haxfred.router)
   .explore('commands');
 
+haxfred.registerConnector(haxfredIrc);
 haxfred.connect();
-
-
-
