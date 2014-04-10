6b8e8f768046b5c0ef98b395c631312a8e2c0241
Sean Zellmer <sean@lejeunerenard.com>
2014-04-09 21:49:09 -0500

    Bare bones event system

--- Adapter.js
+++ Adapter.js
@@ -3,11 +3,7 @@ var interval = null;
 function Adapter(haxfred) {
    haxfred.times = haxfred.times || 0;
    interval = setInterval(function() {
-      if (haxfred.times % 2) {
-         haxfred.emit('baz', haxfred.times);
-      } else {
-         haxfred.emitSequential('baz', haxfred.times);
-      }
+      haxfred.emit('baz', haxfred.times);
       haxfred.times++;
    }, 1000);
 }
