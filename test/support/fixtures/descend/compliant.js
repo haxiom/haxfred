36bec507089cd5ff76eb02747081263e22f49c19
ntomlin <nick.tomlin@gmail.com>
2014-04-12 22:18:41 -0500

    Remove unused modules and tests

--- test/support/fixtures/descend/compliant.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (bot) {
-  bot.prop = 'new';
-
-  if (bot.register) {
-    bot.register('pm',/hello/, function (context) {
-      return "Hey there!" + context.sender;
-    });
-  }
-};
